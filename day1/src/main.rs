054a00e696e8bb5802cfba95e1080c7c3c1d2da1
Filip Kus <kusfilip62@gmail.com>
2025-07-05 11:33:27 +1200

    Completed part two of day 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,6 +1,6 @@
 #![allow(unused_variables)]
 
-use std::{fs::{self, File}, io::{self, BufRead, BufReader}, vec};
+use std::{fs::{self, File}, i32, io::{self, BufRead, BufReader}, vec};
 
 fn add_num_sorted_vec (added_num: i32, vector: Vec<i32>) -> Vec<i32>{
         let vector_length: usize = vector.len();
@@ -94,12 +94,62 @@ fn obtain_vectors_from_file (filename: &str) -> io::Result<(Vec<i32>, Vec<i32>)>
 
     Ok((vector_1, vector_2))
 }
+
+fn num_appearance (vector: &Vec<i32>, num: i32) -> i32 {
+    if let Some(index) = vector.iter().position(|&x| x == num){
+        // println!("{} is located at index {}", num, index);
+
+        let mut all_appearances_found = 0;
+        let mut appearance_count = 0;
+
+        while all_appearances_found == 0 {
+            if index+1 == vector.len() {
+                appearance_count = appearance_count + 1;
+                all_appearances_found = 1;
+            } else if vector[index + appearance_count] == num {
+                appearance_count = appearance_count + 1;
+            } else {
+                all_appearances_found = 1;
+            }
+        }
+        // println!("{}", appearance_count);
+        return  appearance_count as i32;
+    } else{
+        // println!("Couldn't find index");
+        return -1;
+    }
+
+}
+
+fn similarity_score (vector_1: Vec<i32>, vector_2: Vec<i32>) -> i32{
+    let mut similarity_score: i32 = 0;
+    let mut appearance_count: i32;
+
+    for i in 0..vector_1.len(){
+        appearance_count = num_appearance(&vector_2, vector_1[i]);
+        match appearance_count {
+            _ if appearance_count >= 0 => {
+                similarity_score = similarity_score + (appearance_count * vector_1[i]);                
+            },
+            _ => {
+                // println!("Index not found")
+            },
+        }
+               
+        // println!("{}", appearance_count);
+        // println!("{}", similarity_score);
+    }
+    // println!("{}", similarity_score);
+    return similarity_score;
+
+}
 fn main() -> io::Result<()> {
     
     // ---------------------------------------------------------------------------------------------
     // ----------------------------------- PART ONE ------------------------------------------------
     // ---------------------------------------------------------------------------------------------
-    let file = "src/day_1_input_file.txt";
+    // let file = "src/day_1_input_file.txt";
+    let file = "src/test_input_file.txt";
     
    // Obtain columns
     let (column_1, column_2) = obtain_vectors_from_file(file)?;
@@ -107,14 +157,34 @@ fn main() -> io::Result<()> {
     // Sort columns
     let column_1_sorted = sort_vec(column_1);
     let column_2_sorted = sort_vec(column_2);
+    
 
     // Calculate distance between the two columns
     let total_distance = calc_distance(column_1_sorted, column_2_sorted);
 
-    println!("{}", total_distance);
 
     // ---------------------------------------------------------------------------------------------
     // ----------------------------------- PART TWO ------------------------------------------------
     // ---------------------------------------------------------------------------------------------
+    
+    // let file = "src/day_1_input_file.txt";
+    let file = "src/day_1_input_file.txt";
+    
+   // Obtain columns
+    let (column_1, column_2) = obtain_vectors_from_file(file)?;
+    // println!("{:?}", column_1);
+    // println!("{:?}", column_2);
+    // println!("");
+
+    // Sort columns
+    let column_1_sorted = sort_vec(column_1);
+    let column_2_sorted = sort_vec(column_2);    
+    // println!("{:?}", column_1_sorted);
+    // println!("{:?}", column_2_sorted);
+
+    // num_appearance(&column_1_sorted, 3);
+    println!("The similarity score is: {}", similarity_score(column_1_sorted, column_2_sorted));
+    // Final answer is: 22 962 826
+
     Ok(())
 }
