38faa6d839596bd48694c4adb50417c885564827
Filip Kus <kusfilip62@gmail.com>
2025-07-06 15:25:08 +1200

    Completed part two of day 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -38,6 +38,32 @@ fn check_safety(report_1: &Vec<i32>) -> Safety {
     return Safety::Safe;
 }
 
+fn convert_line_to_report (line: String) -> Vec<i32>{
+    let mut previous_space: usize = 0;  
+    let mut report: Vec<i32> = Vec::new();  
+
+    for index in 0..line.len(){        
+        let mut number: &str;
+        if line.chars().nth(index).unwrap() == ' '{
+            
+            if report.is_empty(){
+                number = &line[0..index];
+            } else {
+                number = &line[previous_space+1..index];
+            }
+            
+            report.push(number.parse().unwrap());
+            previous_space = index;
+        }
+
+        if index == line.len()-1 {
+            number = &line[previous_space+1..];
+            report.push(number.parse().unwrap());
+        }
+    }
+    return report;    
+}
+
 fn count_safe_reports (filename: &str) -> io::Result<i32> {
     let file = File::open(filename)?;
     let reader = BufReader::new(file);   
@@ -47,35 +73,31 @@ fn count_safe_reports (filename: &str) -> io::Result<i32> {
     for line in reader.lines(){
         let line = line?;    
 
-        // Go through a single report and extract numbers        
-        let mut previous_space: usize = 0;  
-        let mut report: Vec<i32> = Vec::new();      
-        for index in 0..line.len(){
-            
-            let mut number: &str;
-            if line.chars().nth(index).unwrap() == ' '{
-                
-                if report.is_empty(){
-                    number = &line[0..index];
-                } else {
-                    number = &line[previous_space+1..index];
-                }
-                
-                report.push(number.parse().unwrap());
-                previous_space = index;
-            }
+        // Obtain report vector of type i32 from string
+        let report: Vec<i32>;
+        report = convert_line_to_report(line);
+        println!("{:?}", report);
 
-            if index == line.len()-1 {
-                number = &line[previous_space+1..];
-                report.push(number.parse().unwrap());
-            }
-        }        
-        
-        // Increment total safe reports
-        if check_safety(&report) == Safety::Safe {
+        // Problem Dampener Check
+        if check_safety(&report) == Safety::Unsafe {
+            let mut safety_count = 0;
+            let mut dummy_report: Vec<i32>;
+            for num in 0..report.len(){
+                dummy_report = report.clone();
+                dummy_report.remove(num);
+                if check_safety(&dummy_report) == Safety::Safe{
+                    safety_count += 1;
+                    println!("{:?}", dummy_report);
+                }                
+            }    
+
+            if safety_count >= 1 {
+                total_safe_reports += 1;
+            }        
+        } else if check_safety(&report) == Safety::Safe {
             total_safe_reports += 1;
-        }        
+        }              
     }
-    
+
     Ok(total_safe_reports)
 }
